3e8030977cc8bc4fdc8566a0855dc34ae8a604e9
DK\NPA <npa@sitecore.net>
2018-03-28 16:45:05 +0300

    Native code

---
+++ src/Sitecore.Support.12203/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.12203")]
+[assembly: AssemblyProduct("Sitecore.Support.12203")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.12203/XA/Feature/CreativeExchange/Pipelines/Export/PageProcessing/FixInternalLinks.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Globalization;
+using Sitecore.XA.Feature.CreativeExchange.Pipelines.Export.PageProcessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Sitecore.Support.XA.Feature.CreativeExchange.Pipelines.Export.PageProcessing
+{
+  public class FixInternalLinks : Sitecore.XA.Feature.CreativeExchange.Pipelines.Export.PageProcessing.FixInternalLinks
+  {
+    
+    protected override void OnLinkProcessed(PageProcessingArgs args, string link, Match match)
+    {
+      string str = string.Empty;
+      if (link.Contains<char>('?'))
+      {
+        str = link.Substring(link.IndexOf('?'));
+        link = link.Substring(0, link.IndexOf('?'));
+      }
+      if (!link.EndsWith("/"))
+      {
+        link = link + "/";
+      }
+      link = link + "index.html";
+      link = link + str;
+      link = args.LinkProcessor.RemoveVirtualDirectory(link, args.PageContext.HomeFullPath, args.PageContext.VirtualFolders);
+      link = match.Value.Replace(match.Groups[3].Value, link);
+      args.PageHtml = args.PageHtml.Replace(match.Value, link);
+    }
+  }
+}
